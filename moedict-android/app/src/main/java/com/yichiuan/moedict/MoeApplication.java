64816c79094e60730477165bdb6d7bf24dbddcba
yichiuan <yang.yichiuan@gmail.com>
2017-08-14 19:48:49 +0800

    Configure LeakCanary.

--- moedict-android/app/src/main/java/com/yichiuan/moedict/MoeApplication.java
+++ moedict-android/app/src/main/java/com/yichiuan/moedict/MoeApplication.java
@@ -2,12 +2,21 @@ package com.yichiuan.moedict;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import timber.log.Timber;
 
 public class MoeApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
