82d430470326208c7f4dc807d31f9c8b9a688cc8
yichiuan <yang.yichiuan@gmail.com>
2017-08-17 17:55:22 +0800

    Show search results

--- moedict-android/app/src/main/java/com/yichiuan/moedict/ui/search/SearchActivity.java
+++ moedict-android/app/src/main/java/com/yichiuan/moedict/ui/search/SearchActivity.java
@@ -4,20 +4,49 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.support.v7.widget.RxSearchView;
 import com.yichiuan.moedict.R;
+import com.yichiuan.moedict.data.MoeRepository;
 import com.yichiuan.moedict.ui.main.MainActivity;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import dagger.android.AndroidInjection;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import moe.Index;
+import timber.log.Timber;
 
 public class SearchActivity extends AppCompatActivity {
 
+    @Inject
+    MoeRepository moeRepository;
+
     @BindView(R.id.search_view)
     SearchView searchView;
+
+    @BindView(R.id.recyclerview_search_result)
+    RecyclerView resultRecyclerView;
+
+    @BindView(R.id.textview_search_no_result)
+    TextView noResultTextView;
+
+    private Index index;
+
+    ResultAdapter resultAdapter;
 
     public static void startSearch(Context context) {
         Intent intent = new Intent(context, SearchActivity.class);
@@ -32,7 +61,10 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
 
+        index = moeRepository.getIndex();
+
         setupSearchView();
+        setResultView();
 
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
@@ -40,13 +72,48 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
+    private void setResultView() {
+        resultAdapter = new ResultAdapter(this, index);
+
+        resultRecyclerView.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        resultRecyclerView.addItemDecoration(
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        resultRecyclerView.setHasFixedSize(true);
+        resultRecyclerView.setAdapter(resultAdapter);
+        resultRecyclerView.setVisibility(View.GONE);
+    }
+
     private void setupSearchView() {
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setSubmitButtonEnabled(true);
+
+        RxSearchView.queryTextChanges(searchView)
+                .doOnNext(query -> {
+                    if (query.length() == 0) {
+                        hideResult();
+                    }
+                })
+                .filter(query -> query.length() > 0)
+                .observeOn(Schedulers.computation())
+                .map(query -> index.search(query.toString()))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(results -> {
+                    Timber.d("result count = " + results.size());
+
+                    if (results.isEmpty()) {
+                        showNoResult();
+                    } else {
+                        showResults(results);
+                    }
+                });
     }
 
     private void handleSearchIntent(Intent intent) {
+        if (resultAdapter.getResults().isEmpty()) {
+            return;
+        }
+
         String query = intent.getStringExtra(SearchManager.QUERY);
         Intent show = new Intent(this, MainActivity.class);
         show.putExtra(SearchManager.QUERY, query);
@@ -60,5 +127,24 @@ public class SearchActivity extends AppCompatActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             handleSearchIntent(intent);
         }
+    }
+
+    private void showNoResult() {
+        resultRecyclerView.setVisibility(View.GONE);
+        noResultTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showResults(ArrayList<Integer> results) {
+        resultAdapter.setResults(results);
+        resultRecyclerView.scrollToPosition(0);
+        resultRecyclerView.setVisibility(View.VISIBLE);
+
+        noResultTextView.setVisibility(View.GONE);
+    }
+
+    @MainThread
+    private void hideResult() {
+        resultRecyclerView.setVisibility(View.GONE);
+        noResultTextView.setVisibility(View.GONE);
     }
 }
